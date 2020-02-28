a3ebee899a495c7faf2e33e1fe9f3513a07ff4aa
firatoncu <f.firatoncu@gmail.com>
2020-02-28 14:18:20 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 setup(
-  name = 'foncu1',         # How you named your package folder (MyLib)
-  packages = ['/home/foncu/Desktop/foncu1'],   # Chose the same as "name"
+  name = 'foncu1234',         # How you named your package folder (MyLib)
   version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Global Library Testing by Foncu',   # Give a short description about your library
   author = 'firat oncu',                   # Type in your name
   author_email = 'f.firatoncu@hepsiburada.com',      # Type in your E-Mail
   url = 'https://github.com/firatoncu',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/firatoncu/glfrttest/archive/v0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/firatoncu/foncu1/archive/v0.5.tar.gz',    # I explain this later on
   keywords = ['GLOBAL', 'LIBRARY', 'FIRAT', 'ONCU'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
